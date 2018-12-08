a1acebd8220d212c54d8d998c890b4e8b31f1543
WOLOZ <43232755+WOLOZ@users.noreply.github.com>
2018-12-08 21:05:27 +0800

    Socket

---
+++ socket/src/com/captain/demo/socketThread/SocketServerThread.java
@@ -0,0 +1,42 @@
+package com.captain.demo.socketThread;
+import java.io.*;
+import java.net.Socket;
+
+public class SocketServerThread implements Runnable{
+	private Socket socket;//客户端打开连接
+	public SocketServerThread(Socket socket){
+		this.socket = socket;
+	}
+	public void run(){
+		System.out.println("处理客户端业务线程启动");
+		try{
+			//第一次通讯
+			//获取从客户端传来的信息
+			Reader reader = new InputStreamReader(socket.getInputStream());
+			BufferedReader in = new BufferedReader(reader);
+			String clientMessage = in.readLine();
+			System.out.println("客户端传来的信息："+ clientMessage);
+			//回传信息给客户端
+			PrintWriter out = new PrintWriter(socket.getOutputStream());
+			out.println("这是来自火星的问候");
+			out.flush();
+			//第二次通讯
+			clientMessage = in.readLine();
+			System.out.println("客户端传来的暗号："+clientMessage);
+			out.println("暴风雨就要来了！");
+			out.flush();
+		}catch(IOException e){
+			e.printStackTrace();
+		}finally{
+			try{
+				if(socket != null){
+					socket.close();
+				}	
+			}catch(IOException e){
+				e.printStackTrace();
+			}
+		}
+	}
+	
+
+}
